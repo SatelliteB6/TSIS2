ccee0d0fc9df4560b9606f2d599a4946e5457443
Dimash <dimash_serik@list.ru>
2024-03-09 03:36:54 +0500

    Your commit message here

--- cmd/league/main.go
+++ cmd/league/main.go
@@ -1,18 +1,22 @@
+// cmd/league/main.go
+
 package main
 
 import (
+	"log"
 	"net/http"
 	"github.com/SatelliteB6/TSIS2/pkg/league"
 )
 
 func main() {
-	// Define HTTP routes
 	http.HandleFunc("/champions", league.ListChampions)
 	http.HandleFunc("/champions/create", league.CreateChampion)
 	http.HandleFunc("/champions/get", league.GetChampion)
 	http.HandleFunc("/champions/update", league.UpdateChampion)
 	http.HandleFunc("/champions/delete", league.DeleteChampion)
 
+	// Start the HTTP server
 	port := "8080"
-	http.ListenAndServe(":"+port, nil)
+	log.Printf("Server running on :%s...\n", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
