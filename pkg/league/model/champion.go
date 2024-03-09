570c9f1dd68d190539d0903476a7ac220411f8b5
Dimash <dimash_serik@list.ru>
2024-03-09 05:19:58 +0500

    Your commit message her

--- pkg/league/model/champion.go
+++ pkg/league/model/champion.go
@@ -1,8 +1,8 @@
 package model
 
 type Champion struct {
-	ID         int    `json:"id"`
-	Name       string `json:"name"`
-	Role       string `json:"role"`
-	Difficulty int    `json:"difficulty"`
+	ID    int    `json:"id"`
+	Name  string `json:"name"`
+	Class string `json:"class"`
+	Price int    `json:"price"`
 }
